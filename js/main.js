9c37f77054e69db88317a705cc6971bba3ca3c70
Colin Macrae <csmacrae@gmail.com>
2023-03-01 19:05:46 +0000

    Confirm that the user interface looks natural on both mobile and desktop screensa #1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+document.addEventListener('DOMContentLoaded', $createAndAppend);
+function $createAndAppend() {
+  var $select = document.querySelector('select');
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://developer.nps.gov/api/v1/thingstodo?parkCode=yell&api_key=iSpPR5udcPCzijjVFDgRMe2hLAfepOt9jbFeGFjX');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (var i = 0; i < xhr.response.data.length; i++) {
+      var thingToDo = xhr.response.data[i];
+      var $option = document.createElement('option');
+      $option.textContent = thingToDo.title;
+      $select.appendChild($option);
+    }
+  });
+  xhr.send();
+}
