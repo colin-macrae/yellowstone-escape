4476d23eed2486a5d0fbd9dc0348a89d039c9f3e
Colin Macrae <csmacrae@gmail.com>
2023-03-02 17:33:02 +0000

    Grab the object information from the selected item and put that information into data.viewing #1

--- js/data.js
+++ js/data.js
@@ -1,5 +1,7 @@
 /* exported data */
 var data = {
   savedActivities: [],
-  editing: []
+  editing: [],
+  apiData: {},
+  viewing: {}
 };
--- js/main.js
+++ js/main.js
@@ -4,7 +4,11 @@ function $createAndAppend() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://developer.nps.gov/api/v1/thingstodo?parkCode=yell&api_key=iSpPR5udcPCzijjVFDgRMe2hLAfepOt9jbFeGFjX');
   xhr.responseType = 'json';
+
   xhr.addEventListener('load', function () {
+    console.log(xhr.response);
+    data.apiData = xhr.response.data;
+    // console.log(data.apiData[0].title);
     for (var i = 0; i < xhr.response.data.length; i++) {
       var thingToDo = xhr.response.data[i];
       var $option = document.createElement('option');
@@ -14,3 +18,26 @@ function $createAndAppend() {
   });
   xhr.send();
 }
+
+// document.querySelector('activity-card-title').textContent =
+
+// var $dropdown = document.querySelector('option');
+// $dropdown.addEventListener('change', function() {
+//   console.log(event.target.textContent);
+// });
+var $submit = document.querySelector('form');
+$submit.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // console.log('submitted console');
+  // console.log(document.forms[0].elements.activities.value);
+  // console.log(data.apiData[0].title);
+
+  for (var i = 0; i < data.apiData.length; i++) {
+    if (data.apiData[i].title === document.forms[0].elements.activities.value) {
+      // console.log('match', data.apiData[i]);
+      data.viewing = data.apiData[i];
+      console.log(data.viewing);
+      // return;
+    };
+  }
+});
