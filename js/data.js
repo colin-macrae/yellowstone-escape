1b7835f0da999571061fbe0c4cc00f8d9b2ab648
Colin Macrae <csmacrae@gmail.com>
2023-03-04 18:28:59 +0000

    Create a loop which renders all objects in data.savedActivities using  function and renders the DOM tree for all saved cards #4

--- js/data.js
+++ js/data.js
@@ -7,17 +7,14 @@ var data = {
   viewing: null,
   view: 'home-view'
 };
-// console.log(data);
 
 window.addEventListener('beforeunload', toJSON);
 function toJSON(e) {
   var dataStringifiedJson = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', dataStringifiedJson);
 }
-// console.log(data.nextSavedActivityId);
 
 if (localStorage.getItem('javascript-local-storage')) {
   var jsonParsedFromStorage = localStorage.getItem('javascript-local-storage');
   data = JSON.parse(jsonParsedFromStorage);
 }
-// console.log(data.nextSavedActivityId);
